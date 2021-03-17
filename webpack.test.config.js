60aa321df0402d9df2dfbcd56880c11573cc4883
Yuantao <work@pandorastudio.cn>
2021-03-17 16:47:29 +0800

    优化了若干方法

---
+++ webpack.test.config.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+module.exports = {
+  watch: true,
+  watchOptions: {
+    poll: 1000, // 每秒询问多少次
+    aggregateTimeout: 500, //防抖 多少毫秒后再次触发
+  },
+  entry: path.resolve(__dirname, "Pandora.js"),
+  output: {
+    filename: "Pandora.min.js",
+  },
+};
